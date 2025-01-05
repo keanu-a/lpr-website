3ef979f7941d8c41d5b5355034962eedd0a44694
keanu-a <keanucaloua@gmail.com>
2025-01-05 12:51:32 -0800

    Created nav bar for desktop

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,91 @@
 import { NavLink } from 'react-router';
 import crest from '../assets/crest.png';
 
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+
+const navItems: { title: string; links: { name: string; href: string }[] }[] = [
+  {
+    title: 'About',
+    links: [
+      {
+        name: 'About us',
+        href: '/about-us',
+      },
+      {
+        name: 'Upcoming Events',
+        href: '/upcoming-events',
+      },
+      {
+        name: 'Stepping',
+        href: '/stepping',
+      },
+      {
+        name: 'Academics',
+        href: '/academics',
+      },
+    ],
+  },
+  {
+    title: 'Membership',
+    links: [
+      {
+        name: 'Membership',
+        href: '/membership',
+      },
+      {
+        name: 'Expansion',
+        href: '/expansion',
+      },
+      {
+        name: 'Incorporation, Chartering, and Non-Profit',
+        href: '/incorporation',
+      },
+    ],
+  },
+  {
+    title: 'Brothers',
+    links: [
+      {
+        name: 'Founding Chapter',
+        href: '/founding-chapter',
+      },
+      {
+        name: 'Active House',
+        href: '/active-house',
+      },
+      {
+        name: 'Alumni Chapter',
+        href: '/alumni-chapter',
+      },
+      {
+        name: 'National Board',
+        href: '/national-board',
+      },
+    ],
+  },
+  {
+    title: 'Philanthropy',
+    links: [
+      {
+        name: 'Local Philanthropies',
+        href: '/local-philanthropies',
+      },
+    ],
+  },
+];
+
 export default function Nav() {
   return (
-    <nav className="flex justify-around my-4 items-center">
-      <div className="flex items-center">
+    <nav className="flex justify-around my-4 mx-auto items-center">
+      <NavLink to="/" className="flex items-center">
         <img className="w-28" src={crest} alt="Lambda Psi Rho Crest" />
 
         <div>
@@ -12,22 +93,30 @@ export default function Nav() {
           <hr />
           <p>Fraternity, Inc.</p>
         </div>
-      </div>
+      </NavLink>
 
-      <ul className="flex justify-around w-1/2">
-        <li>
-          <NavLink to="about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="membership">Membership</NavLink>
-        </li>
-        <li>
-          <NavLink to="brothers">Brothers</NavLink>
-        </li>
-        <li>
-          <NavLink to="philanthropy">Philanthropy</NavLink>
-        </li>
-      </ul>
+      <NavigationMenu>
+        <NavigationMenuList>
+          {navItems.map((item) => (
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
+
+              <NavigationMenuContent>
+                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                  {item.links.map((link) => (
+                    <NavigationMenuLink
+                      href={link.href}
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      {link.name}
+                    </NavigationMenuLink>
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
     </nav>
   );
 }
