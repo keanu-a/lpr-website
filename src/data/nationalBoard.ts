953e971415b36e8ff87791f9a5b1644b8e05ce1b
keanu-a <keanucaloua@gmail.com>
2025-01-25 15:36:06 -0800

    Added national board skeleton

---
+++ src/data/nationalBoard.ts
@@ -0,0 +1,58 @@
+export const term: string = 'Fall 2024 - Spring 2026';
+
+export const nationalBoard: {
+  name: string;
+  position: string;
+  email: string;
+  class: string;
+  graduation: string;
+  degree: string;
+  bio: string;
+  photo?: string;
+}[] = [
+  {
+    name: 'Danny Phung',
+    position: 'National Board President',
+    email: 'nb.president@lambdapsirho.org',
+    class: 'Lambda',
+    graduation: 'Fall 2017',
+    degree: 'Psychology',
+    bio: 'yup',
+  },
+  {
+    name: 'Jacob Davis',
+    position: 'National Board Vice President',
+    email: 'nb.vicepresident@lambdapsirho.org',
+    class: 'Kappa',
+    graduation: 'Fall 2017',
+    degree: 'Psycology',
+    bio: 'yup',
+  },
+  {
+    name: 'Christian Corabo',
+    position: 'National Board Treasurer',
+    email: 'nb.treasurer@lambdapsirho.org',
+    class: 'Mu',
+    graduation: 'Fall 2017',
+    degree: 'Psycology',
+    bio: 'yup',
+  },
+  {
+    name: 'Raul Esteves',
+    position: 'National Board Historian',
+    email: 'nb.historian@lambdapsirho.org',
+    class: 'Lambda',
+    graduation: 'Fall 2017',
+    degree: 'Psycology',
+    bio: 'yup',
+  },
+  {
+    name: 'Keanu Aloua',
+    position: 'National Board Secretary',
+    email: 'nb.secretary@lambdapsirho.org',
+    class: 'Sigma',
+    graduation: 'Spring 2024',
+    degree: 'B.S. in Computer Science',
+    bio: 'yup',
+  },
+];
--- src/pages/Brothers/NationalBoard.tsx
+++ src/pages/Brothers/NationalBoard.tsx
@@ -1,3 +1,30 @@
+import { term, nationalBoard } from '@/data/nationalBoard';
+
 export default function NationalBoard() {
-  return <div>NationalBoard</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="flex justify-center text-2xl mt-4 font-bold md:text-4xl">
+        National Board
+      </h1>
+
+      <h2 className="flex justify-center text-xl">{term}</h2>
+
+      <div className="flex flex-col gap-4">
+        {nationalBoard.map((data, nationalBoardIndex) => (
+          <div key={nationalBoardIndex}>
+            <h4>{data.position}</h4>
+            <h3>{data.name}</h3>
+
+            <h5>{data.email}</h5>
+            <p>
+              {data.class} class: Graduated {data.graduation} with a{' '}
+              {data.degree}
+            </p>
+
+            <p>{data.bio}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
