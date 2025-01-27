95bc11bf52ba2a4542cf969dea3ba13f76d85e24
keanu-a <keanucaloua@gmail.com>
2025-01-26 17:04:17 -0800

    Added photo placeholders for each class

--- src/pages/Brothers/FoundingChapter.tsx
+++ src/pages/Brothers/FoundingChapter.tsx
@@ -37,14 +37,20 @@ export default function FoundingChapter() {
                 {data.greekLetter} - {data.className}
               </p>
             </AccordionTrigger>
-            <AccordionContent>
-              <ul className="flex flex-col gap-2">
-                <div>
+            <AccordionContent
+              className={`flex flex-col justify-around items-center gap-2 md:flex-row ${
+                index % 2 === 0 && 'md:flex-row-reverse'
+              }`}
+            >
+              <div className="w-96 h-72 bg-black" />
+              <div className="flex flex-col gap-2 p-2">
+                <Title title={data.className} subtitle={data.greekLetter} />
+                <ul className="flex flex-wrap justify-center gap-4 md:flex-col">
                   {data.brothers.map((name, brotherIndex) => (
                     <li key={brotherIndex}>{name}</li>
                   ))}
-                </div>
-              </ul>
+                </ul>
+              </div>
             </AccordionContent>
           </AccordionItem>
         ))}
