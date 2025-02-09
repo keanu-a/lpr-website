25f2e6fa1a1a0b24c293b1fa9ed87e4a077599db
Keanu C <keanucaloua@gmail.com>
2025-02-08 16:58:06 -0800

    Fixed home scroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,20 @@
 import Nav from "../components/Nav";
 import heroBg from "../assets/active-house.jpg";
 import Footer from "@/components/Footer";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <div
         className="relative h-screen bg-cover bg-center"
         style={{ backgroundImage: `url(${heroBg})` }}
       >
-        <div className="absolute inset-0 bg-black opacity-70 z-0"></div>
+        <div className="absolute inset-0 bg-black opacity-60 z-0"></div>
         <Nav className="relative z-15" />
 
         <div className="absolute top-1/2 transform -translate-y-1/2 inset-0 flex flex-col items-center justify-center h-fit text-white z-5">
