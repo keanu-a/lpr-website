d1b082b2b78ff9d573ca9346d3b0879ec91ee6df
keanu-a <keanucaloua@gmail.com>
2025-02-17 21:03:47 -0800

    Added videos to the stepping page

---
+++ src/data/videos.ts
@@ -0,0 +1,27 @@
+export const steppingPageVideos: { title: string; src: string }[] = [
+  {
+    title: '2018 MGC Yardshow (Fall)',
+    src: 'https://www.youtube.com/embed/Ox1xyZEDMPc?autoplay=1',
+  },
+  {
+    title: 'Sigma Class Presentation (Fall 2019)',
+    src: 'https://www.youtube.com/embed/mR4v6rWMZzU',
+  },
+  {
+    title: '2018 MGC Yardshow (Spring)',
+    src: 'https://www.youtube.com/embed/nHAb_zU522w',
+  },
+  {
+    title: '2017 Fall New Member Presentation',
+    src: 'https://www.youtube.com/embed/8nanfkuwU7c',
+  },
+  {
+    title: '2016 Fall New Member Presentation',
+    src: 'https://www.youtube.com/embed/Kq8Pb3VogMY',
+  },
+
+  {
+    title: '2016 Spring New Member Presentation',
+    src: 'https://www.youtube.com/embed/D_5kvqV7a3M',
+  },
+];
--- src/pages/About/Stepping.tsx
+++ src/pages/About/Stepping.tsx
@@ -1,3 +1,25 @@
+import { steppingPageVideos } from '@/data/videos';
+
+const VIDEO_WIDTH = 640;
+const VIDEO_HEIGHT = 360;
+
 export default function Stepping() {
-  return <div>Stepping</div>;
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <h1 className="text-2xl font-bold md:text-4xl">Stepping</h1>
+
+      <div className="flex flex-col gap-4">
+        {steppingPageVideos.map((data, videoIdx) => (
+          <iframe
+            key={videoIdx}
+            title={data.title}
+            src={data.src}
+            width={VIDEO_WIDTH}
+            height={VIDEO_HEIGHT}
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        ))}
+      </div>
+    </div>
+  );
 }
